7f61c02d05ad1ec406eed3fd8486f99c41e37212
Sumit Singh Karki <sumitkarki1114@gmail.com>
2025-09-15 18:03:46 +0530

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import 'prismjs/themes/prism-tomorrow.css'
-import './styles/App.css'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
