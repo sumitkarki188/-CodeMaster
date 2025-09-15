556aa5b1fd411f39b6c10981d1b5df46c02f1a65
Sumit Singh Karki <sumitkarki1114@gmail.com>
2025-09-15 18:00:16 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,28 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-      }
-    }
-  },
-  css: {
-    devSourcemap: true,
-    preprocessorOptions: {
-      css: {
-        charset: false
-      }
-    }
+    open: true
   },
   build: {
     outDir: 'dist',
-    sourcemap: true,
-    cssCodeSplit: false
-  }
+    assetsDir: 'assets',
+    sourcemap: false
+  },
+  base: '/'
 })
