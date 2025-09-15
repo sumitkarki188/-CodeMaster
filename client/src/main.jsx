21cfa22084c9f916004d99d580a51ccc23074b3f
Sumit Singh Karki <sumitkarki1114@gmail.com>
2025-09-15 18:09:44 +0530

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,14 +4,6 @@ import App from './App.jsx'
 import './index.css'
 
 
-import 'prismjs/themes/prism.css'
-import 'prismjs/components/prism-core'
-import 'prismjs/components/prism-clike'
-import 'prismjs/components/prism-javascript'
-import 'prismjs/components/prism-python'
-import 'prismjs/components/prism-java'
-import 'prismjs/components/prism-cpp'
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
