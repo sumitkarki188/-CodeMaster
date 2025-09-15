c58c29e0896f96a48659af437bb33dc8d59b244e
Sumit Singh Karki <sumitkarki1114@gmail.com>
2025-09-15 18:24:10 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const mysql = require('mysql2');
-const axios = require('axios');
+const axios = require('axios');..
 
 const app = express();
 const PORT = process.env.PORT || 5000;
